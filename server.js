bc7644b4e93828f3a9654335db947107c29ffc08
surajks <surajkskulal@gmail.com>
2025-07-02 16:03:40 +0530

    robo

--- server.js
+++ server.js
@@ -9,10 +9,20 @@ app.set("views", path.join(__dirname, "views")); //set file
 app.use(express.static(path.join(__dirname, "/public"))); //for using statc file like css,js files
 
 
-app.listen("8080", ()=>{
-console.log("listening the PORT 8080");
-});
+
 
 app.get("/", (req,res) => {
     res.render("listings/index.ejs");
+});
+
+app.get("/education", (req,res)=>{
+    res.render("listings/education.ejs");
+});
+
+app.get("/project", (req,res)=>{
+    res.render("listings/project.ejs");
+});
+
+app.listen("8080", ()=>{
+console.log("listening the PORT 8080");
 });
